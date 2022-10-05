701d8b4453acb9632c09231ada24a60f8777a47d
captcoaldale <102894355+captcoaldale@users.noreply.github.com>
2022-10-05 00:20:22 -0600

    2022-10-04 First pushAdd project files.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -30,38 +30,61 @@ namespace PointOfSale
         private void InitializeComponent()
         {
             this.gbxClient = new System.Windows.Forms.GroupBox();
-            this.cmbClient = new System.Windows.Forms.ComboBox();
             this.lblClient = new System.Windows.Forms.Label();
+            this.cmbClient = new System.Windows.Forms.ComboBox();
+            this.cmbAgent = new System.Windows.Forms.ComboBox();
+            this.lblAgent = new System.Windows.Forms.Label();
             this.gbxClient.SuspendLayout();
             this.SuspendLayout();
             // 
             // gbxClient
             // 
+            this.gbxClient.Controls.Add(this.lblAgent);
+            this.gbxClient.Controls.Add(this.cmbAgent);
             this.gbxClient.Controls.Add(this.lblClient);
             this.gbxClient.Controls.Add(this.cmbClient);
+            this.gbxClient.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.gbxClient.Location = new System.Drawing.Point(12, 13);
             this.gbxClient.Name = "gbxClient";
-            this.gbxClient.Size = new System.Drawing.Size(442, 84);
+            this.gbxClient.Size = new System.Drawing.Size(442, 124);
             this.gbxClient.TabIndex = 0;
             this.gbxClient.TabStop = false;
-            this.gbxClient.Text = "Client";
-            // 
-            // cmbClient
-            // 
-            this.cmbClient.FormattingEnabled = true;
-            this.cmbClient.Location = new System.Drawing.Point(86, 29);
-            this.cmbClient.Name = "cmbClient";
-            this.cmbClient.Size = new System.Drawing.Size(286, 21);
-            this.cmbClient.TabIndex = 0;
+            this.gbxClient.Text = "Personnel";
             // 
             // lblClient
             // 
             this.lblClient.AutoSize = true;
             this.lblClient.Location = new System.Drawing.Point(4, 30);
             this.lblClient.Name = "lblClient";
-            this.lblClient.Size = new System.Drawing.Size(36, 13);
+            this.lblClient.Size = new System.Drawing.Size(53, 20);
             this.lblClient.TabIndex = 1;
             this.lblClient.Text = "Client:";
+            // 
+            // cmbClient
+            // 
+            this.cmbClient.FormattingEnabled = true;
+            this.cmbClient.Location = new System.Drawing.Point(86, 29);
+            this.cmbClient.Name = "cmbClient";
+            this.cmbClient.Size = new System.Drawing.Size(286, 28);
+            this.cmbClient.TabIndex = 0;
+            this.cmbClient.SelectedIndexChanged += new System.EventHandler(this.cmbClient_SelectedIndexChanged);
+            // 
+            // cmbAgent
+            // 
+            this.cmbAgent.FormattingEnabled = true;
+            this.cmbAgent.Location = new System.Drawing.Point(86, 63);
+            this.cmbAgent.Name = "cmbAgent";
+            this.cmbAgent.Size = new System.Drawing.Size(286, 28);
+            this.cmbAgent.TabIndex = 2;
+            // 
+            // lblAgent
+            // 
+            this.lblAgent.AutoSize = true;
+            this.lblAgent.Location = new System.Drawing.Point(6, 71);
+            this.lblAgent.Name = "lblAgent";
+            this.lblAgent.Size = new System.Drawing.Size(52, 20);
+            this.lblAgent.TabIndex = 3;
+            this.lblAgent.Text = "Agent";
             // 
             // Form1
             // 
@@ -82,6 +105,8 @@ namespace PointOfSale
         private System.Windows.Forms.GroupBox gbxClient;
         private System.Windows.Forms.Label lblClient;
         private System.Windows.Forms.ComboBox cmbClient;
+        private System.Windows.Forms.Label lblAgent;
+        private System.Windows.Forms.ComboBox cmbAgent;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PointOfSale.DataObjects;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,6 +8,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+
+
 
 namespace PointOfSale
 {
@@ -18,21 +21,38 @@ namespace PointOfSale
         public Form1()
         {
             InitializeComponent();
-            InitializeCombos();
+            this.Load += Form1_Load;
         }
+
         private void InitializeCombos()
         {
-            foreach(Control ctl in this.Controls)
+           foreach(Control ctl in gbxClient.Controls)
             {
-                string prompt = "Select Client";
-                if(ctl is ComboBox)
+               if(ctl is ComboBox)
                 {
+                    string prompt = "";
+                    string query = "";
+                    string value = "";
+                    string display = "";
                     ComboBox cmb = (ComboBox)ctl;
-                    switch(cmb.Name)
+                    switch (cmb.Name)
                     {
-                        case"cmbClient":
+                        case "cmbClient":
+                            query = "SELECT company_pk, company_name FROM tbl_company WHERE is_client = true";
+                            value = "company_pk";
+                            display = "company_name";
+                            prompt = "Select Client";
                             break;
                         case "cmbAgent":
+                            query = "SELECT p.personnel_pk, concat(p.first_name,' ',p.last_name,' -- ',o.office_desc) " +
+                                "AS display " +
+                                "FROM tbl_personnel p " +
+                                "INNER JOIN tbl_office o " +
+                                "ON p.office_fk = o.office_pk " +
+                                "WHERE p.function_fk = 5";
+                            value = "personnel_pk";
+                            display = "display";
+                            prompt = "Select Agent";
                             break;
                         case "cmbProjectType":
                             break;
@@ -49,11 +69,61 @@ namespace PointOfSale
 
 
                     }
+                    MySQL_Connection conn = new MySQL_Connection();
+                    //Create a binding source
+                    conn.OpenConnection();
+                    BindingSource bindingSource = new BindingSource(conn.DataTableGet(query), null);
+                    conn.CloseConnection();
+                    // set properties
+                    cmb.DisplayMember = display;
+                    cmb.ValueMember = value;
                     cmb.Text = prompt;
-                    cmb.ForeColor = Color.LightGray;
+                    cmb.ForeColor = Color.Gray;
+                    // must set last or breaks ...
+                    cmb.DataSource = bindingSource;
                 }
 
             }
+        }
+
+        private void cmbClient_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            MessageBox.Show("I am selected.");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            /*
+            this.dvgField = new DataGridView();
+            this.dvgParameter = new DataGridView();
+            this.dvgSoluables = new DataGridView();
+            this.dvgTotals = new DataGridView();
+            this.dvgSaturationIndex = new DataGridView();
+            this.dvgCorrosion = new DataGridView();
+            btnSave.Visible = false;
+            */
+            InitializeCombos();
+            this.FormClosing += new FormClosingEventHandler(Form1_Closing);
+        }
+
+        private void Form1_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            // Determine if text has changed in the textbox by comparing to original text.
+            /*
+            if (textBox1.Text != strMyOriginalText)
+            {
+                // Display a MsgBox asking the user to save changes or abort.
+                if (MessageBox.Show("Do you want to save changes to your text?", "My Application",
+                    MessageBoxButtons.YesNo) == DialogResult.Yes)
+                {
+                    // Cancel the Closing event from closing the form.
+                    e.Cancel = true;
+                    // Call method to save file...
+                }
+            }
+            */
+            // cleanup, first release memory...
+            System.Windows.Forms.Application.Exit();
         }
     }
 }
